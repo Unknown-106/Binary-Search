bd978af2dda4e376ff590836bc8b7def82df1f65
Unknown-106 <azhiamin10@gmail.com>
2025-11-06 13:29:04 +0000

    Add files via upload

---
+++ Binary Search.py
@@ -0,0 +1,33 @@
+# Binary Search
+# Azhi Amin
+# 05/11/2025
+# OCR AS Computer Science
+
+#For a binary search, list must be SORTED!
+wlist = [i for i in range(1,101)]
+print(wlist)
+
+found = False
+searchTerm = 42
+
+#first and last pointers
+first = 0
+last = len(wlist) - 1
+
+#
+while found == False and last >= first:
+    midpoint = (first + last) // 2
+    if searchTerm == wlist[midpoint]:
+        found = True
+        break
+    else:
+        if searchTerm > wlist[midpoint]:
+            first = midpoint + 1
+        elif searchTerm < wlist[midpoint]:
+            last = midpoint - 1
+
+#
+if Found == True:
+    print(f"Data item '{searchTerm}' found at position {midpoint}.")
+else:
+    print(f"Data element '{searchTerm}' not found")
